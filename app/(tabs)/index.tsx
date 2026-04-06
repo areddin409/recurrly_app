7de58adb44ad6cb011ca0be11ff75b3a05b4fc5f
Andrew Reddin <areddin409@gmail.com>
2026-04-05 20:56:44 -0500

    feat: add custom fonts and update layout for onboarding screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -11,33 +11,24 @@ import { Text } from "react-native"
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background   p-5">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
+      <Text className="text-5xl font-sans-extrabold ">Home</Text>
       <Link
         href="/onboarding"
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
       >
         <Text>Go to Onboarding</Text>
       </Link>
       <Link
         href="/(auth)/sign-in"
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
       >
         <Text>Go to Sign In</Text>
       </Link>
       <Link
         href="/(auth)/sign-up"
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
       >
         <Text>Go to Sign Up</Text>
-      </Link>
-
-      <Link
-        href="/subscriptions/spotify"
-        className="mt-4 rounded bg-primary text-white p-4"
-      >
-        <Text>Spotify Subscription</Text>
       </Link>
     </SafeAreaView>
   )
